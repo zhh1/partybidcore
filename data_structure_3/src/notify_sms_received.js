22a09506c87e41fb2a3c7b04becdc5d184c4bd32
jjq <zhh1>
2014-09-20 11:45:22 +0800

    succeed to run sign_up_spec.js of data_structure_3

--- data_structure_3/src/notify_sms_received.js
+++ data_structure_3/src/notify_sms_received.js
@@ -1,3 +1,7 @@
-function notify_sms_received(){
-
+function notify_sms_received(sms){
+    var result = sms.messages[0].message.replace(/\s/g,'');
+    if(result.search(/bm/i) == 0) {
+        var sign_up = new SignUp(sms);
+        sign_up.sign_up();
+    }
 }
---
+++ data_structure_3/src/sign_up.js
@@ -0,0 +1,36 @@
+function SignUp(sms) {
+    var result = sms.messages[0].message.replace(/\s/g,'');
+    this.phone = sms.messages[0].phone;
+    this.name = result.substr(2);
+    this.activity_id = localStorage.current_activity;
+}
+
+SignUp.set_sign_ups = function(sign_up){
+    localStorage.sign_ups = JSON.stringify(sign_up);
+};
+
+SignUp.get_sign_ups = function() {
+    return JSON.parse(localStorage.sign_ups);
+};
+
+SignUp.prototype.judge_is_signing_up = function() {
+    return localStorage.is_signing_up == "true";
+};
+
+SignUp.prototype.is_repeat = function() {
+    return _(SignUp.get_sign_ups()).some(function(item) {
+        return item.phone == this.phone;
+    },this)
+};
+
+SignUp.prototype.sign_up = function() {
+    if(!this.judge_is_signing_up()) {
+        return;
+    }
+    if(!this.is_repeat()) {
+        var sign_ups = SignUp.get_sign_ups();
+        sign_ups.push(this);
+        SignUp.set_sign_ups(sign_ups);
+    }
+};
+
